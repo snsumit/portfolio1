28aa02d832028c33928802568fe066e7b90a93ac
snsumit <141250140+snsumit@users.noreply.github.com>
2023-08-02 21:26:09 +0530

    added

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text" , {
+   strings :["Frontend Devloper" , "Backend Devloper" , "Web designer"] ,
+   typeSpeed:100,
+   backSpeed:100,
+   backDelay:1000,
+   loop:true
+});
